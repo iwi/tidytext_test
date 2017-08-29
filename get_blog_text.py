dddffc3cc2c7e5494cb66d1e7a3f2c5f7bb5ec27
iwi <iwi@tuta.io>
2017-08-29 20:45:53 +0100

    feat: Add getter

--- get_blog_text.py
+++ get_blog_text.py
@@ -8,7 +8,7 @@ import sys
 
 def get_text_only(url):
     page = requests.get(url)
-    parsed = BeautifulSoup(page, 'html.parser')
+    parsed = BeautifulSoup(page.content, 'html.parser')
     all_p = parsed.find_all('p')
     text_list = list(map(BeautifulSoup.get_text, all_p))
     text = ''.join(text_list)
@@ -17,25 +17,25 @@ def get_text_only(url):
 
 if __name__ == '__main__':
     # Urls to get
-    urls = {
-        's02e01': 'https://medium.com/@drryandunn/weeknotes-s01e09-7135e6d4c4d7',
-        's01e08': 'https://medium.com/@drryandunn/weeknotes-s01e08-c50fef5ace1a',
-        's01e07': 'https://medium.com/@drryandunn/weeknotes-s01e07-378ef801057d',
-        's01e06': 'https://medium.com/@drryandunn/weeknotes-s01e06-1f119808daa4',
-        's01e05': 'https://medium.com/@drryandunn/weeknotes-s01e05-6d0020c9b335',
-        's01e04': 'https://productforthepeople.xyz/weeknotes-s01e04-6a21a20ac6b0',
-        's01e03': 'https://productforthepeople.xyz/weeknotes-s01e02-674e81f44797',
-        's01e02': 'https://productforthepeople.xyz/weeknotes-s01e03-2caaa58a0547',
-        's01e01': 'https://productforthepeople.xyz/weeknotes-s01-e01-af39311da6c4'
-        }
+    urls = [
+        'https://medium.com/@drryandunn/weeknotes-s02-e01-68af74d9e96b',
+        'https://medium.com/@drryandunn/weeknotes-s01e09-7135e6d4c4d7',
+        'https://medium.com/@drryandunn/weeknotes-s01e08-c50fef5ace1a',
+        'https://medium.com/@drryandunn/weeknotes-s01e07-378ef801057d',
+        'https://medium.com/@drryandunn/weeknotes-s01e06-1f119808daa4',
+        'https://medium.com/@drryandunn/weeknotes-s01e05-6d0020c9b335',
+        'https://productforthepeople.xyz/weeknotes-s01e04-6a21a20ac6b0',
+        'https://productforthepeople.xyz/weeknotes-s01e03-2caaa58a0547',
+        'https://productforthepeople.xyz/weeknotes-s01e02-674e81f44797',
+        'https://productforthepeople.xyz/weeknotes-s01-e01-af39311da6c4'
+    ]
 
     texts = []
 
-    for episode in urls:
-        text = get_text_only(episode)
+    for url in urls:
+        text = get_text_only(url)
         texts.append(text)
 
-with open("weeknotes_ryan.csv", "w", newline = "") as fout:
-    writer = csv.writer(fout, delimiter = ',')
-    writer.writerrows(texts)
-
+    with open("weeknotes_ryan.csv", "w", newline="") as fout:
+        writer = csv.writer(fout, delimiter=',')
+        writer.writerow(texts)
